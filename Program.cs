d776b321d3923449f5cde4a082febaabf0751b0c
Gor Rustamyan <gor.rustamyan@gmail.com>
2018-11-08 18:10:43 +0400

    Created new sea-war project intial structure

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cshaprtraining_sea_war
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
