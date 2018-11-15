721aeec0e60de51465ee8699401db6ef33a7b65f
VardanChichyan <chichyan97@mail.ru>
2018-11-15 18:07:03 +0400

    inplemented CanBePlaced function

--- GameEngine.CanBePlaced.cs
+++ GameEngine.CanBePlaced.cs
@@ -9,7 +9,33 @@ namespace Foundation.Hub256.Seawar
         /// </summary>
         public bool CanBePlaced(Cell[,] field, Ship ship)
         {
-            throw new NotImplementedException();            
+
+            for (int i = -1; i < (int)ship.Size+1; i++)
+            {
+                if (ship.Orientation == ShipOrientation.Vertical)
+                {
+                    if (field[ship.Start.X, ship.Start.Y + i] == Cell.Ship
+                    || (ship.Start.X+1<10 && field[ship.Start.X+1, ship.Start.Y + i] == Cell.Ship) 
+                    ||(ship.Start.X-1>0 && field[ship.Start.X-1, ship.Start.Y + i] == Cell.Ship) )
+                    {
+                        return false;
+                    }
+                    
+                }
+
+                else
+                {
+                    if (ship.Orientation == ShipOrientation.Horizontal)
+                    {
+                        if (field[ship.Start.X + i, ship.Start.Y] == Cell.Ship)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+            return true;     
         }
+
     }
 }
