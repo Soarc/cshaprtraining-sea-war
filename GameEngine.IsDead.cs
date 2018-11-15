fe55cecd17f0ef1c36f08da45d91f36dacb2423a
root <root@localhost.localdomain>
2018-11-15 17:53:48 +0400

    Implemented IsDead function

--- GameEngine.IsDead.cs
+++ GameEngine.IsDead.cs
@@ -9,6 +9,25 @@ namespace Foundation.Hub256.Seawar
         /// </summary>
         public bool IsDead(Cell[,] field, Coordinates coord)
         {
+			Ship boat;
+			boat = FindShip(field, coord);
+			int q;
+
+			if (boat.Orientation == Horizontal)
+				for (int i = boat.Start.X; i <= boat.End.X; i++)
+					if (i != HittedShip)
+						break;
+					else
+						i = DeadShip;
+			else if(boat.Orientation == Vertical)
+				for (int i = boat.Start.Y; i <= boat.End.Y; i++)
+                    if (i != HittedShip)
+                        break;
+                    else
+                        i = DeadShip;
+
+
+
             throw new NotImplementedException();            
         }
     }
