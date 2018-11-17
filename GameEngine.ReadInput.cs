0fc90e90a023b82689f6333a4eddc9fc7ababf1c
Gor Rustamyan <gor.rustamyan@gmail.com>
2018-11-17 18:52:44 +0400

    Some fixes.

--- GameEngine.ReadInput.cs
+++ GameEngine.ReadInput.cs
@@ -10,54 +10,42 @@ namespace Foundation.Hub256.Seawar
         public Coordinates ReadInput()
         {
             Coordinates coordinates = new Coordinates();
-            bool Corect = false;
-            while (!Corect)
+            while (true)
             {
                 string a = Console.ReadLine();
-                if (a.Length > 2 && a.Length < 2)
+                if (a.Length > 2 || a.Length < 2)
                 {
                     Console.WriteLine("oops:entered symbols count I'ts wrong");
+                    continue;
                 }
-                else
+
+
+                char b = a[0];
+                char c = a[1];
+
+                //Int32.TryParse(b.ToString(), out int d);
+
+
+                if (!Int32.TryParse(c.ToString(), out int e) || !(e <= 10 && e >= 1))
                 {
-
-                    char b = a[0];
-                    char c = a[1];
-
-                    //Int32.TryParse(b.ToString(), out int d);
-                    Int32.TryParse(c.ToString(), out int e);
-
-                      if (e >= '0' && e <= '9')
-
-                      {
-                        if ('a' <= b && b <= 'z')
-                        {
-                            coordinates.Y = b - 'a';
-                            Corect = true;
-                        }
-
-                        else if ('A' <= b && b <= 'Z')
-                        {
-                            coordinates.Y = b - 'A';
-                        }
-
-
-                        coordinates.X = e - 1;
-                        if (Corect)
-                            return coordinates;
-                      }
-                    else
-                    {
-                        Console.WriteLine("oops: The second  symbol must be a number");
-                    }
-                
-
+                    Console.WriteLine("oops: The second  symbol must be a number");
+                    continue;
                 }
-            }
-            return default(Coordinates);
-        }
-            
-    }
-    
+                coordinates.X = e - 1;
+                if ('a' <= b && b <= 'z')
+                {
+                    coordinates.Y = b - 'a';
+                    return coordinates;
+                }
 
+                if ('A' <= b && b <= 'Z')
+                {
+                    coordinates.Y = b - 'A';
+                    return coordinates;
+                }
+
+                Console.WriteLine("oops: First symbol should be letter from A-Z.");
+            }
+        }
+    }
 }
