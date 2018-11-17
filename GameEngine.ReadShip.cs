942da647e851fd3fa4ceab4d839dc6d6b27839aa
eduard.gasparyan <eduard.gasparyan20@gmail.com>
2018-11-17 18:09:50 +0300

    ReadShip

--- GameEngine.ReadShip.cs
+++ GameEngine.ReadShip.cs
@@ -9,7 +9,65 @@ namespace Foundation.Hub256.Seawar
         /// </summary>
         public Ship ReadShip(ShipSize size)
         {
-            throw new NotImplementedException();            
+            Ship ship=new Ship();
+            string a=Console.ReadLine();
+            ship.Start.X=a[0];
+            ship.Start.Y=a[1];
+            ship.End.X=a[3];
+            ship.End.Y=a[4];
+
+ if (ship.End.X-ship.Start.X==0)
+ {
+     
+     if (ship.End.Y-ship.Start.Y+1==1)
+     {
+        return ship;   
+     }
+    else  if (ship.End.Y-ship.Start.Y+1==2)
+     {
+         return ship;
+     }
+    else  if (ship.End.Y-ship.Start.Y+1==3)
+     {
+         return ship;
+     }
+    else  if (ship.End.Y-ship.Start.Y+1==4)
+     {
+         return ship;
+     }
+     else
+     {
+        return 0;
+     }
+ }
+ else if (ship.End.Y-ship.Start.Y==0)
+ {
+      if (ship.End.X-ship.Start.X+1==1)
+     {
+         return ship;
+     }
+    else  if (ship.End.X-ship.Start.X+1==2)
+     {
+         return ship;
+     }
+    else  if (ship.End.X-ship.Start.X+1==3)
+     {
+         return ship;
+     }
+    else  if (ship.End.X-ship.Start.X+1==4)
+     {
+         return ship;
+     }
+     else 
+     {
+return 0;
+     }
+ }
+else
+{
+return 0;
+}
+ 
         }
     }
 }
