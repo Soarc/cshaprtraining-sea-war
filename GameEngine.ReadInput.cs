5a3e92b396932a5b3dfd1a96dd0c51f9de49cb3b
Diana <manukyan.dinulik@mail.ru>
2018-11-17 17:45:55 +0400

    Implemented readinput function.

--- GameEngine.ReadInput.cs
+++ GameEngine.ReadInput.cs
@@ -9,7 +9,46 @@ namespace Foundation.Hub256.Seawar
         /// </summary>
         public Coordinates ReadInput()
         {
-            throw new NotImplementedException();            
+            Coordinates coordinates = new Coordinates();
+            bool Corect = false;
+            while (!Corect)
+            {
+                string a = (string)Console.ReadLine();
+                if (a.Length > 2)
+                {
+                    Console.WriteLine("oops: mutqagrvac simvolneri qanaky shat e");
+                }
+                else
+                {
+
+                    char b = a[0];
+                    char c = a[1];
+
+                    //Int32.TryParse(b.ToString(), out int d);
+                    Int32.TryParse(c.ToString(), out int e);
+
+
+                    if ('a' <= b && b <= 'z')
+                    {
+                        coordinates.Y = b - 'a';
+                        Corect = true;
+                    }
+
+                    else if ('A' <= b && b <= 'Z')
+                    {
+                        coordinates.Y = b - 'A';
+                    }
+
+
+                    coordinates.X = e - 1;
+                    if (Corect)
+                        return coordinates;
+                }
+            }
+            return default(Coordinates);
         }
+            
     }
+    
+
 }
