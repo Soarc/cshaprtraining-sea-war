926e61c87fa7502547f9a51ec3d77e579ebd6b72
VardanChichyan <chichyan97@mail.ru>
2018-11-17 18:37:28 +0400

    Implemented CanBePlaced function

--- GameEngine.CanBePlaced.cs
+++ GameEngine.CanBePlaced.cs
@@ -14,9 +14,9 @@ namespace Foundation.Hub256.Seawar
             {
                 if (ship.Orientation == ShipOrientation.Vertical)
                 {
-                    if (((ship.Start.Y+i>=0 && ship.Start.Y+i<10 ) && field[ship.Start.X, ship.Start.Y + i] == Cell.Ship) 
+                    if ((ship.Start.Y+i>=0 && ship.Start.Y+i<10 ) && (( field[ship.Start.X, ship.Start.Y + i] == Cell.Ship) 
                     || (ship.Start.X+1<10 && field[ship.Start.X+1, ship.Start.Y + i] == Cell.Ship) 
-                    ||(ship.Start.X-1>=0 && field[ship.Start.X-1, ship.Start.Y + i] == Cell.Ship) )
+                    ||(ship.Start.X-1>=0 && field[ship.Start.X-1, ship.Start.Y + i] == Cell.Ship))) 
                     {
                         return false;
                     }
@@ -27,9 +27,9 @@ namespace Foundation.Hub256.Seawar
                 {
                     if (ship.Orientation == ShipOrientation.Horizontal)
                     {
-                        if (((ship.Start.X+i>=0 && ship.Start.X+i<10)&& field[ship.Start.X + i, ship.Start.Y] == Cell.Ship)
+                        if ((ship.Start.X+i>=0 && ship.Start.X+i<10) && ((field[ship.Start.X + i, ship.Start.Y] == Cell.Ship)
                         ||(ship.Start.Y+1<10 && field[ship.Start.X+i,ship.Start.Y+1]==Cell.Ship)
-                        ||(ship.Start.Y-1>=0 && field[ship.Start.X+i,ship.Start.Y-1]==Cell.Ship))
+                        ||(ship.Start.Y-1>=0 && field[ship.Start.X+i,ship.Start.Y-1]==Cell.Ship)))
 
                         {
                             return false;
